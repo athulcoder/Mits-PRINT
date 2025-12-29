40b8322efb81eb54231b5d582844348949a259d5
athulcoder <athul.sin.cos.tan@gmail.com>
2025-12-29 23:56:26 +0530

    printer status api

--- application/app/api/printer/route.js
+++ application/app/api/printer/route.js
@@ -2,18 +2,23 @@ import { NextResponse } from "next/server";
 
 export async function POST(req){
 
-    const body = await req.json();
     
     try{
+
+    const body = await req.json();
+
     const {SECRET_KEY} = body;
 
 
     if(SECRET_KEY != process.env.SECRET_KEY){
-        return NextResponse.json({message:"YOU ARE NOT AUTH TO GET THIS"})
+        throw e
     }
 
+    const {printerId , printerStatus , printerReason} =body;
 
-    return NextResponse.json({message:"U are a super user"})
+    return NextResponse.json({message:"U are a super user" , printerId, printerStatus,printerReason})
+
+
     }catch(error){
         
         return NextResponse.json({message:"unexpected error occured "});
