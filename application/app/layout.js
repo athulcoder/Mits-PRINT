a403f47f82451871037e7abbe578ff1f4fdad852
athulcoder <athul.sin.cos.tan@gmail.com>
2026-01-13 18:04:18 +0530

    favicon

--- application/app/layout.js
+++ application/app/layout.js
@@ -2,9 +2,18 @@
 import Script from "next/script";
 import "./globals.css";
 export const metadata = {
-  title: "MITS PRINT ",
-  description: "",
-};
+  title: "MITS Print",
+  description: "Smart way to print lab records & project reports",
+
+  manifest: "/site.webmanifest",
+
+  icons: {
+    icon: "/icon.png",
+    shortcut: "/favicon.ico",
+    apple: "/apple-icon.png",
+    apple: "/apple-icon.png",
+  },
+}
 
 export default function RootLayout({ children }) {
   return (
